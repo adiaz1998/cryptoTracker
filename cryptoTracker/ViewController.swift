12c704536d4502d2654a1bee5226912c4fc4e3d5
Alan Diaz <adiaz98.2020@gmail.com>
2021-11-17 18:19:28 -0500

    Initial Commit

---
+++ cryptoTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cryptoTracker
+//
+//  Created by Alan Díaz on 11/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
