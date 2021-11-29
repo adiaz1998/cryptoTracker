a5357634f64f93bfd49f3d3dd066aeea75cae41a
Alan Diaz <adiaz98.2020@gmail.com>
2021-11-28 21:39:39 -0500

    Added icon

--- cryptoTracker/ViewController.swift
+++ cryptoTracker/ViewController.swift
@@ -8,12 +8,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    
+    
+        if let symbol = textField.text {
+        
+        getData(symbol : symbol)
+        
+        }
+        
+        
     }
+    
+    var url = "https://min-api.cryptocompare.com/data/price?tsyms=USD"
+    
+    func getData(symbol : String){
+        
+        url = "\(url)&fsym=\(symbol)"
+        
+        // 1. Initialize the URL
+        
+        guard let url = URL(string: url) else {return}
+        
+    //2. Initialize task and URL Session
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            // 3. Check Optional
+            guard let data = data, error == nil else {return}
+            
+            print("Data Received")
+            
+            do{
+                
+                let Result = try JSONDecoder().decode(APIResponse.self, from: data)
+                
+                print(Result.USD)
+                
+                DispatchQueue.main.async {
+                
+                self.outputLabel.text = "\(Result.USD)"
+                
+                }
+                    
+            } catch {
+                
+                print(error.localizedDescription)
+                
+            }
+     
+        }
+        
+        // Resume the task
+        task.resume()
 
+        
+            // CLOSURE - create a function with function within function
+    }
+    
+    // 5. Create a structure and mention what the parameters are
+    
+    struct APIResponse : Codable {
+        
+        let USD : Float
+        
+        
+    }
+    
 
 }
+
 
